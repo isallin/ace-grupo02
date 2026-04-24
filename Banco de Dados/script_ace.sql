032d2e64eecfc15a9b2f33eb377dae1e7845866e
SammRibeiro <samuel.florenco@sptech.school>
2026-04-24 19:04:37 -0300

    add: tabela de logs no script

--- Banco de Dados/script_ace.sql
+++ Banco de Dados/script_ace.sql
@@ -1,251 +1,298 @@
 -- -----------------------------------------------------
--- Schema projetoace 1.1
+-- Schema projetoace
 -- -----------------------------------------------------
-CREATE
-DATABASE projetoace;
-USE
-projetoace ;
+CREATE DATABASE projetoace;
+USE projetoace;
+
 -- -----------------------------------------------------
 -- Table organizacao
 -- -----------------------------------------------------
-CREATE TABLE organizacao
-(
+CREATE TABLE organizacao (
     idorganizacao INT PRIMARY KEY AUTO_INCREMENT,
-    nome          VARCHAR(45),
-    contato       VARCHAR(45),
-    codAtivacao   VARCHAR(45)
+    nome VARCHAR(45),
+    contato VARCHAR(45),
+    codAtivacao VARCHAR(45)
 );
 
 -- -----------------------------------------------------
 -- Table usuarios
 -- -----------------------------------------------------
-CREATE TABLE usuarios
-(
-    idusuarios    INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE usuarios (
+    idusuarios INT PRIMARY KEY AUTO_INCREMENT,
     nome_completo VARCHAR(45),
-    nickname      VARCHAR(45),
-    email         VARCHAR(100),
-    senha         VARCHAR(50),
-    funcao        VARCHAR(20),
+    nickname VARCHAR(45),
+    email VARCHAR(100),
+    senha VARCHAR(50),
+    funcao VARCHAR(20),
     organizacaofk INT,
-    CONSTRAINT chk_funcao CHECK (funcao IN ('player', 'coach', 'outros')),
+
+    CONSTRAINT chk_funcao
+        CHECK (funcao IN ('player','coach','outros')),
+
     CONSTRAINT fk_usuario_organizacao
         FOREIGN KEY (organizacaofk)
-            REFERENCES organizacao (idorganizacao)
+        REFERENCES organizacao(idorganizacao)
 );
 
 -- -----------------------------------------------------
 -- Table post
 -- -----------------------------------------------------
-CREATE TABLE post
-(
-    idpost     INT PRIMARY KEY AUTO_INCREMENT,
-    texto      VARCHAR(200),
-    data       DATE,
+CREATE TABLE post (
+    idpost INT PRIMARY KEY AUTO_INCREMENT,
+    texto VARCHAR(200),
+    data DATE,
     usuariosfk INT,
-    CONSTRAINT fk_post_usuarios
+
+    CONSTRAINT fk_post_usuario
         FOREIGN KEY (usuariosfk)
-            REFERENCES usuarios (idusuarios)
+        REFERENCES usuarios(idusuarios)
 );
 
 -- -----------------------------------------------------
 -- Table agente
 -- -----------------------------------------------------
-CREATE TABLE agente
-(
-    idagente   INT PRIMARY KEY AUTO_INCREMENT,
-    nome       VARCHAR(45) UNIQUE,
-    classe     VARCHAR(45),
-    link_image VARCHAR(45),
-    descricao  VARCHAR(200), -- usado para o post
-    postfk     INT,
+CREATE TABLE agente (
+    idagente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) UNIQUE,
+    classe VARCHAR(45),
+    link_image VARCHAR(100),
+    descricao VARCHAR(200),
+    postfk INT,
+
     CONSTRAINT fk_agente_post
         FOREIGN KEY (postfk)
-            REFERENCES post (idpost)
+        REFERENCES post(idpost)
 );
 
 -- -----------------------------------------------------
 -- Table mapa
 -- -----------------------------------------------------
-CREATE TABLE mapa
-(
-    idmapa     INT PRIMARY KEY AUTO_INCREMENT,
-    nome       VARCHAR(45) NOT NULL UNIQUE,
-    link_image VARCHAR(45) NULL,
-    postfk     INT NULL,
+CREATE TABLE mapa (
+    idmapa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) UNIQUE,
+    link_image VARCHAR(100),
+    postfk INT,
+
     CONSTRAINT fk_mapa_post
         FOREIGN KEY (postfk)
-            REFERENCES post (idpost)
+        REFERENCES post(idpost)
 );
 
 -- -----------------------------------------------------
 -- Table time
 -- -----------------------------------------------------
-CREATE TABLE time
-(
+CREATE TABLE time (
     idtime INT PRIMARY KEY AUTO_INCREMENT,
-    nome   VARCHAR(45) NOT NULL UNIQUE
+    nome VARCHAR(45) UNIQUE
 );
 
 -- -----------------------------------------------------
 -- Table partida
 -- -----------------------------------------------------
-CREATE TABLE partida
-(
-    idpartida    INT PRIMARY KEY AUTO_INCREMENT,
-    nome_partida VARCHAR(45) UNIQUE,
-    campeonato   VARCHAR(45),
-    etapa        VARCHAR(45),
-    mapaFk       INT,
+CREATE TABLE partida (
+    idpartida INT PRIMARY KEY AUTO_INCREMENT,
+    nome_partida VARCHAR(100) UNIQUE,
+    campeonato VARCHAR(60),
+    etapa VARCHAR(60),
+
+    mapaFk INT,
+    vencedorFk INT,
+
+    placarA INT,
+    placarB INT,
+
     CONSTRAINT fk_partida_mapa
         FOREIGN KEY (mapaFk)
-            REFERENCES mapa (idmapa)
+        REFERENCES mapa(idmapa),
+
+    CONSTRAINT fk_partida_vencedor
+        FOREIGN KEY (vencedorFk)
+        REFERENCES time(idtime)
 );
 
 -- -----------------------------------------------------
 -- Table desempenho
 -- -----------------------------------------------------
-CREATE TABLE desempenho
-(
-    id        INT PRIMARY KEY AUTO_INCREMENT,
-    kills     INT,
-    deaths    INT,
-    assists   INT,
-    partidafk INT,
-    agentefk  INT,
-    timefk    INT,
+CREATE TABLE desempenho (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+
+    kills INT,
+    deaths INT,
+    assists INT,
+
+    partidaFk INT,
+    agenteFk INT,
+    timeFk INT,
+
     CONSTRAINT fk_desempenho_partida
-        FOREIGN KEY (partidafk)
-            REFERENCES partida (idpartida),
+        FOREIGN KEY (partidaFk)
+        REFERENCES partida(idpartida),
+
     CONSTRAINT fk_desempenho_agente
-        FOREIGN KEY (agentefk)
-            REFERENCES agente (idagente),
+        FOREIGN KEY (agenteFk)
+        REFERENCES agente(idagente),
+
     CONSTRAINT fk_desempenho_time
-        FOREIGN KEY (timefk)
-            REFERENCES time (idtime)
+        FOREIGN KEY (timeFk)
+        REFERENCES time(idtime)
 );
 
 -- -----------------------------------------------------
 -- Table composicao
 -- -----------------------------------------------------
--- -----------------------------------------------------
--- Table composicao
--- -----------------------------------------------------
-CREATE TABLE composicao
-(
+CREATE TABLE composicao (
     idcomposicao INT PRIMARY KEY AUTO_INCREMENT,
-    timefk       INT,
-    partidafk    INT,
+
+    timeFk INT,
+    partidaFk INT,
 
     CONSTRAINT uk_composicao
-        UNIQUE (timefk, partidafk),
+        UNIQUE (timeFk, partidaFk),
 
     CONSTRAINT fk_composicao_time
-        FOREIGN KEY (timefk)
-            REFERENCES time (idtime),
+        FOREIGN KEY (timeFk)
+        REFERENCES time(idtime),
 
     CONSTRAINT fk_composicao_partida
-        FOREIGN KEY (partidafk)
-            REFERENCES partida (idpartida)
+        FOREIGN KEY (partidaFk)
+        REFERENCES partida(idpartida)
 );
 
 -- -----------------------------------------------------
 -- Table composicao_agente
 -- -----------------------------------------------------
-CREATE TABLE composicao_agente
-(
-    composicaofk INT,
-    agentefk     INT,
-    PRIMARY KEY (agentefk, composicaofk),
+CREATE TABLE composicao_agente (
+
+    composicaoFk INT,
+    agenteFk INT,
+
+    PRIMARY KEY (composicaoFk, agenteFk),
+
     CONSTRAINT fk_composicaoagente_composicao
-        FOREIGN KEY (composicaofk)
-            REFERENCES composicao (idcomposicao),
+        FOREIGN KEY (composicaoFk)
+        REFERENCES composicao(idcomposicao),
+
     CONSTRAINT fk_composicaoagente_agente
-        FOREIGN KEY (agentefk)
-            REFERENCES agente (idagente)
+        FOREIGN KEY (agenteFk)
+        REFERENCES agente(idagente)
 );
 
 -- -----------------------------------------------------
 -- Table estatistica_composicao
 -- -----------------------------------------------------
-CREATE TABLE estatistica_composicao
-(
-    id             INT PRIMARY KEY AUTO_INCREMENT,
-    win_rate       DECIMAL,
-    pick_rate      DECIMAL,
+CREATE TABLE estatistica_composicao (
+
+    id INT PRIMARY KEY AUTO_INCREMENT,
+
+    win_rate DECIMAL(5,2),
+    pick_rate DECIMAL(5,2),
     total_partidas INT,
-    composicaofk   INT,
-    CONSTRAINT fk_estatisticacomposicao_composicao
-        FOREIGN KEY (composicaofk)
-            REFERENCES composicao (idcomposicao)
+
+    composicaoFk INT UNIQUE,
+
+    CONSTRAINT fk_estatistica_composicao
+        FOREIGN KEY (composicaoFk)
+        REFERENCES composicao(idcomposicao)
 );
 
 -- -----------------------------------------------------
--- Table comentarios
+-- Table log
 -- -----------------------------------------------------
-CREATE TABLE comentarios
-(
-    idpost     INT,
-    idusuarios INT,
-    data       DATE,
-    texto      VARCHAR(200),
-    PRIMARY KEY (idpost, idusuarios),
-    CONSTRAINT fk_comentarios_post
-        FOREIGN KEY (idpost)
-            REFERENCES post (idpost),
-    CONSTRAINT fk_comentarios_usuarios
-        FOREIGN KEY (idusuarios)
-            REFERENCES usuarios (idusuarios)
+CREATE TABLE log_etl (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
+    mensagem VARCHAR(255)
 );
 
-
+-- -----------------------------------------------------
+-- Testes ETL
+-- -----------------------------------------------------
+SELECT COUNT(*) AS total FROM mapa;
+SELECT COUNT(*) AS total FROM agente;
+SELECT COUNT(*) AS total FROM time;
+SELECT COUNT(*) AS total FROM partida;
+SELECT COUNT(*) AS total FROM desempenho;
+SELECT COUNT(*) AS total FROM composicao;
+SELECT COUNT(*) AS total FROM composicao_agente;
+SELECT COUNT(*) AS total FROM estatistica_composicao;
 
 -- -----------------------------------------------------
--- Testes do ETL
+-- Consulta desempenho
 -- -----------------------------------------------------
-SELECT COUNT(*) AS total
-FROM mapa;
-SELECT COUNT(*) AS total
-FROM agente;
-SELECT COUNT(*) AS total
-FROM time;
-SELECT COUNT(*) AS total
-FROM partida;
-SELECT COUNT(*) AS total
-FROM desempenho;
-
-SELECT p.idpartida,
-       p.nome_partida,
-       p.campeonato,
-       p.etapa,
-       m.nome AS mapa
-FROM partida p
-         JOIN mapa m
-              ON p.mapaFk = m.idmapa;
-
-SELECT d.id,
-       p.nome_partida,
-       t.nome AS time,
+SELECT
+d.id,
+p.nome_partida,
+t.nome AS time,
 a.nome AS agente,
 d.kills,
 d.deaths,
 d.assists
 FROM desempenho d
-    JOIN partida p
-ON d.partidaFk = p.idpartida
-    JOIN time t
-    ON d.timeFk = t.idtime
-    JOIN agente a
-    ON d.agenteFk = a.idagente
-    LIMIT 500;
+JOIN partida p ON d.partidaFk = p.idpartida
+JOIN time t ON d.timeFk = t.idtime
+JOIN agente a ON d.agenteFk = a.idagente
+LIMIT 100;
 
-SELECT c.idcomposicao,
-       t.nome,
-       p.nome_partida,
-       a.nome agente
+-- -----------------------------------------------------
+-- Consulta composição
+-- -----------------------------------------------------
+SELECT
+c.idcomposicao,
+t.nome AS time,
+p.nome_partida,
+a.nome AS agente
 FROM composicao c
-         JOIN time t ON c.timeFk = t.idtime
-         JOIN partida p ON c.partidaFk = p.idpartida
-         JOIN composicao_agente ca ON ca.composicaoFk = c.idcomposicao
-         JOIN agente a ON a.idagente = ca.agenteFk
+JOIN time t ON c.timeFk = t.idtime
+JOIN partida p ON c.partidaFk = p.idpartida
+JOIN composicao_agente ca ON ca.composicaoFk = c.idcomposicao
+JOIN agente a ON a.idagente = ca.agenteFk
 ORDER BY c.idcomposicao;
+
+-- -----------------------------------------------------
+-- Consulta estatística
+-- -----------------------------------------------------
+SELECT
+e.id,
+t.nome,
+p.nome_partida,
+e.win_rate,
+e.pick_rate,
+e.total_partidas
+FROM estatistica_composicao e
+JOIN composicao c ON e.composicaoFk = c.idcomposicao
+JOIN time t ON c.timeFk = t.idtime
+JOIN partida p ON c.partidaFk = p.idpartida;
+
+-- -----------------------------------------------------
+-- Estatísticas das composições
+-- -----------------------------------------------------
+SELECT
+    ec.id,
+    c.idcomposicao,
+
+    t.nome AS time,
+    p.nome_partida,
+    m.nome AS mapa,
+
+    ec.win_rate,
+    ec.pick_rate,
+    ec.total_partidas
+
+FROM estatistica_composicao ec
+
+JOIN composicao c
+    ON ec.composicaoFk = c.idcomposicao
+
+JOIN time t
+    ON c.timeFk = t.idtime
+
+JOIN partida p
+    ON c.partidaFk = p.idpartida
+
+JOIN mapa m
+    ON p.mapaFk = m.idmapa
+
+ORDER BY ec.win_rate DESC,
+         ec.pick_rate DESC;
+
+
